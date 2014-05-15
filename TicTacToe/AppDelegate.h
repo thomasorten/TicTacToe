ed09024c9457adc369e7bbe505fd9296c0f9ad93
Thomas Orten <thomas.orten@gmail.com>
2014-05-15 20:29:05 +0200

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Orten, Thomas on 15.05.14.
+//  Copyright (c) 2014 Orten, Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Orten, Thomas on 15.05.14.
+//  Copyright (c) 2014 Orten, Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
